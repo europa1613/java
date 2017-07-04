b707880288492423330fd192ad0e8f53bd11a26e
Arvind <europa1613@users.noreply.github.com>
2017-07-04 15:20:03 -0400

    create static reference directly

--- CoreJava/src/com/corejava/ObjectInStaticBlockDemo.java
+++ CoreJava/src/com/corejava/ObjectInStaticBlockDemo.java
@@ -2,12 +2,18 @@ package com.corejava;
 
 public class ObjectInStaticBlockDemo {
 
+	static ObjectInStaticBlockDemo obj2;
+
 	static {
 		ObjectInStaticBlockDemo obj = new ObjectInStaticBlockDemo();
-		System.out.println(obj);
+		System.out.println("obj: " + obj);
+
+		System.out.println("obj2 Before instantiation: " + ObjectInStaticBlockDemo.obj2);
+		ObjectInStaticBlockDemo.obj2 = new ObjectInStaticBlockDemo();
+		System.out.println("obj2 after instantiation: " + ObjectInStaticBlockDemo.obj2);
 	}
-	
+
 	public static void main(String[] args) {
-		
+		System.out.println("obj2 main: " + ObjectInStaticBlockDemo.obj2);
 	}
 }
