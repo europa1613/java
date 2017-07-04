908a579b9c45fe03f0ce77248c92286934b606b0
Arvind <europa1613@users.noreply.github.com>
2017-07-04 15:52:25 -0400

    this keyword and create object in static method

--- CoreJava/src/com/corejava/ObjectInStaticBlockDemo.java
+++ CoreJava/src/com/corejava/ObjectInStaticBlockDemo.java
@@ -11,9 +11,15 @@ public class ObjectInStaticBlockDemo {
 		System.out.println("obj2 Before instantiation: " + ObjectInStaticBlockDemo.obj2);
 		ObjectInStaticBlockDemo.obj2 = new ObjectInStaticBlockDemo();
 		System.out.println("obj2 after instantiation: " + ObjectInStaticBlockDemo.obj2);
+		
+		ObjectInStaticBlockDemo.init();
 	}
 
 	public static void main(String[] args) {
 		System.out.println("obj2 main: " + ObjectInStaticBlockDemo.obj2);
+	}
+	
+	static void init() {
+		ObjectInStaticBlockDemo.obj2 = new ObjectInStaticBlockDemo();
 	}
 }
