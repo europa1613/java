b0b6bd6c07ed2fc3ec2ded24823926dc08f6fda2
Arvind <europa1613@users.noreply.github.com>
2017-07-04 15:15:40 -0400

    Create obj in static block

---
+++ CoreJava/src/com/corejava/ObjectInStaticBlockDemo.java
@@ -0,0 +1,13 @@
+package com.corejava;
+
+public class ObjectInStaticBlockDemo {
+
+	static {
+		ObjectInStaticBlockDemo obj = new ObjectInStaticBlockDemo();
+		System.out.println(obj);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
